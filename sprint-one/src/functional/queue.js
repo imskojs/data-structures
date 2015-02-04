ad498553287b0fd6dc800327c4bab81b90fa8888
Seunghoon Ko <imskojs@gmail.com>
2015-02-05 06:24:25 +0900

    initial

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,9 @@
 var Queue = function(){
-  var someInstance = {};
+  var someInstance = {
+    length: 0,
+    head: 0,
+    tail: 0
+  };
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +11,22 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[this.tail] = value;
+    this.tail++;
+    this.length++;
   };
 
   someInstance.dequeue = function(){
+    var results = storage[this.head];
+    delete storage[this.head];
+    this.head++;
+    this.length--;
+    this.length = Math.max(0, this.length);
+    return results; 
   };
 
   someInstance.size = function(){
+    return this.length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,29 @@
 var Stack = function(){
-  var someInstance = {};
+  var someInstance = {length: 0};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    this[this.length] = value;
+    this.length++;
   };
 
   someInstance.pop = function(){
+    var something = this[this.length-1];
+    delete this[this.length-1];
+    this.length--;
+    this.length = Math.max(0, this.length);
+    return something
   };
 
   someInstance.size = function(){
+    return this.length
   };
 
   return someInstance;
 };
+
+
+//var newStack = Stack();
